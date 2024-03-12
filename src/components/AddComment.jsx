1f7e5d7f26e5beb0d85eddf11a72c07bccdb1f2b
stephenbowyer <github@ste.widearea.org>
2024-03-12 14:08:35 +0000

    Removed optimistic rendering for posted comments

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -9,33 +9,11 @@ const AddComment = ({article_id, setComments}) => {
     const handleSubmit = (event) => {
         event.preventDefault();
         if (newCommentBody.length > 0){
-            const newComment = {
-                comment_id: 'newcomment',
-                body: newCommentBody,
-                article_id: article_id,
-                author: username,
-                votes: 0,
-                created_at: Date.now()
-            }
-            setComments((originalComments) => {
-                const newComments = [newComment, ...originalComments];
-                return newComments;
-            });
-            setNewCommentBody('');
-            postComment(article_id, username, newComment.body).then(({comment}) => {
-                setComments((originalComments) => {
-                    const newComments = [...originalComments];
-                    return newComments.map((currComment) => {
-                        if (currComment.comment_id === 'newcomment'){
-                            return comment;
-                        }
-                        return currComment;
-                    });
-                });
-                setFeedback('');
+            setFeedback('');
+            postComment(article_id, username, newCommentBody).then(({comment}) => {
+                setComments((originalComments) => [comment, ...originalComments]);
+                setNewCommentBody('');
             }).catch(() => {
-                setNewCommentBody(newComment.body);
-                setComments((comments) => comments.filter((currComment) => currComment.comment_id !== 'newcomment'));
                 setFeedback('Error posting');
             })
         }
