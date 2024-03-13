db358583abf921ab048a26234c3d58d23cd6139d
stephenbowyer <github@ste.widearea.org>
2024-03-13 11:05:02 +0000

    Update axios param passing and changed api name

--- src/utils.js
+++ src/utils.js
@@ -1,61 +1,54 @@
 import axios from 'axios'
 
-const marketApi = axios.create({
+const newsApi = axios.create({
     baseURL: 'https://nc-news-40gp.onrender.com/api'
 })
 
 export const fetchArticles = (topic = '') => {
-    const queryParams = [];
-    let queryString = "";
-    if (topic){
-        queryParams.push(`topic=${topic}`)
-    }
-    if (queryParams.length > 0){
-        queryString = "?"+queryParams.join('&');
-    }
-    return marketApi.get(`/articles${queryString}`)
+    const queryParams = {params: {topic}}
+    return newsApi.get('/articles', queryParams)
     .then((response) => {
         return response.data
     });
 }
 
 export const fetchArticle = (articleId) => {
-    return marketApi.get(`/articles/${articleId}`)
+    return newsApi.get(`/articles/${articleId}`)
     .then((response) => {
         return response.data
     })
 }
 
 export const upVoteArticle = (articleId, votes = 1) => {
-    return marketApi.patch(`/articles/${articleId}`, {inc_votes: votes})
+    return newsApi.patch(`/articles/${articleId}`, {inc_votes: votes})
     .then((response) => {
         return response.data
     })
 }
 
 export const fetchComments = (articleId) => {
-    return marketApi.get(`/articles/${articleId}/comments`)
+    return newsApi.get(`/articles/${articleId}/comments`)
     .then((response) => {
         return response.data
     })
 }
 
 export const postComment = (articleId, username, body) => {
-    return marketApi.post(`/articles/${articleId}/comments`, {username, body})
+    return newsApi.post(`/articles/${articleId}/comments`, {username, body})
     .then((response) => {
         return response.data
     })   
 }
 
 export const deleteComment = (commentId) => {
-    return marketApi.delete(`/comments/${commentId}`)
+    return newsApi.delete(`/comments/${commentId}`)
     .then((response) => {
         return response.data
     })
 }
 
 export const fetchTopics = () => {
-    return marketApi.get('/topics')
+    return newsApi.get('/topics')
     .then((response) => {
         return response.data
     })
